6c3cad304dc9df1359b586dce5bf427e5a8ac9c2
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-05 16:19:31 +0800

    change args

--- scripts/SEG/context.sh
+++ scripts/SEG/context.sh
@@ -1,5 +1,5 @@
 cd /home/hongyu/Visual-RAG-LLaVA-Med
-file=IDRiD_49
+file=20170504152807089
 python VRAG_seg.py \
     --image-org ./segmentation/${file}.png \
     --image-seg ./segmentation/${file}.jpg 
