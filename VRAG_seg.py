55efeadd46bb1088bd30e51851a8275c2522051d
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-05 15:23:12 +0800

    add vrag only use seg image

---
+++ VRAG_seg.py
@@ -0,0 +1,121 @@
+import argparse
+import torch
+import os
+import json
+from tqdm import tqdm
+
+from PIL import Image
+import math
+from transformers import set_seed, logging
+
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria, process_images
+from llama_index.core import (ServiceContext, 
+                               SimpleDirectoryReader,
+                               SimpleDirectoryReader,
+                               StorageContext,
+                               Settings)
+from llama_index.core.schema import ImageNode
+from llama_index.core.schema import ImageDocument
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core.response.notebook_utils import display_source_node
+from llama_index.core.indices.multi_modal.base import MultiModalVectorStoreIndex
+from llama_index.embeddings.clip import ClipEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+
+class VRAG_seg():
+    def __init__(self, args):
+        self.model_path = args.model_path
+        model_name = get_model_name_from_path(self.model_path)
+        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(
+            self.model_path, args.model_base, model_name
+        )
+        self.image_folder = args.image_folder
+        self.prompt_str = (
+            "Given the provided information, including retrieved contents and metadata, \
+            accurately and precisely answer the query without any additional prior knowledge.\n"
+            "Please ensure honesty and responsibility, refraining from any racist or sexist remarks.\n"
+            "---------------------\n"
+            "Context: {context_str}\n"     ## 将上下文信息放进去
+            "---------------------\n"
+            "Query: {query_str}\n"
+            "Answer: "
+        )
+        self.context_str = "In segmentation image (the second image), the light red part means microaneurysm, the dark red part means hemorrhage, the yellow part means exudates, the white part means cotton wool spots."
+        self.query_str = args.query_str
+        self.image_org = args.image_org
+        self.image_seg = args.image_seg
+        
+    def inference(self):
+        set_seed(0)
+        disable_torch_init()
+        # add context and get input ids
+        prompt = self.prompt_str.format(
+            context_str=self.context_str,
+            query_str=self.query_str, 
+        )
+        qs = prompt.replace(DEFAULT_IMAGE_TOKEN, '').strip()
+        if self.model.config.mm_use_im_start_end:
+            qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+            
+        conv = conv_templates[args.conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+        
+        # add image and seg
+        image_org = Image.open(self.image_org)
+        image_seg = Image.open(self.image_seg)
+        image_tensor = process_images([image_org, image_seg], self.image_processor, self.model.config)[0]
+        
+        # do inference
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(keywords, self.tokenizer, input_ids)
+        
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                input_ids,
+                images=image_tensor.unsqueeze(0).half().cuda(),
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                num_beams=args.num_beams,
+                # no_repeat_ngram_size=3,
+                max_new_tokens=1024,
+                use_cache=True)
+
+        outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        return outputs
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", type=str, default="/home/hongyu/Visual-RAG-LLaVA-Med/Model/llava-med-v1.5-mistral-7b")
+    parser.add_argument("--model-base", type=str, default=None)
+    parser.add_argument("--question-file", type=str, default="/home/hongyu/Visual-RAG-LLaVA-Med/data/eye_diag.json")
+    parser.add_argument("--conv-mode", type=str, default="vicuna_v1")
+    parser.add_argument("--image-folder", type=str, default="./segmentation/")
+    parser.add_argument("--image-org", type=str, default="./segmentation/IDRiD_49.png")
+    parser.add_argument("--image-seg", type=str, default="./segmentation/IDRiD_49.jpg")
+    parser.add_argument("--num-chunks", type=int, default=1)
+    parser.add_argument("--chunk-idx", type=int, default=0)
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--top_p", type=float, default=None)
+    parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--meta-data", type=str, default="/home/hongyu/Visual-RAG-LLaVA-Med/data/segmentation.json")
+    args = parser.parse_args()
+    
+    vrag = VRAG_seg(args)
+    # print(vrag.inference())
+    # vrag.build_index()
+    test_img = "/home/hongyu/DDR/lesion_segmentation/test/image/007-1789-100.jpg"
+    query_str_0 = "Can you describe the image in details?"
+    query_str_1 = "what's the diagnosis?"
+    vrag.inference_rag(query_str_0, test_img)
