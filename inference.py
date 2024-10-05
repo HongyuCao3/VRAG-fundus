3de024c393f93393a350f5a5fe39ec040d0c3a78
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-05 13:40:05 +0800

    add index builder

--- inference.py
+++ inference.py
@@ -13,59 +13,91 @@ from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
 from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria, process_images
-from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (ServiceContext, 
+                               SimpleDirectoryReader,
+                               SimpleDirectoryReader,
+                               StorageContext,
+                               Settings)
+from llama_index.core.schema import ImageNode
+from llama_index.core.schema import ImageDocument
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core.response.notebook_utils import display_source_node
+from llama_index.core.indices.multi_modal.base import MultiModalVectorStoreIndex
+from llama_index.embeddings.clip import ClipEmbedding
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.replicate import Replicate
-from transformers import AutoTokenizer
 
-def inference(args):
-    set_seed(0)
-    disable_torch_init()
-    model_path = args.model_path
-    model_name = get_model_name_from_path(model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(
-        model_path, args.model_base, model_name
-    )
-    with open(args.question_file, 'r', encoding='utf-8') as file:
-        questions = json.load(file)
-    # questions = [json.loads(q) for q in open(args.question_file,"r")]
-    for line in tqdm(questions):
-        qs = line["text"].replace(DEFAULT_IMAGE_TOKEN, '').strip()
-        image_file = line["image"]
-        if model.config.mm_use_im_start_end:
-            qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
-        else:
-            qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+class VRAG():
+    def __init__(self, args):
+        self.model_path = args.model_path
+        model_name = get_model_name_from_path(self.model_path)
+        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(
+            self.model_path, args.model_base, model_name
+        )
+        self.image_folder = args.image_folder
+        
+    def build_index(self,):
+        # read meta data
+        document = []
+        with open(args.meta_data, "r", encoding="UTF-8") as f_m:
+            meta_data = json.load(f_m)
+        for d in meta_data:
+            caption = d["discription"]
+            img_path = self.image_folder + d["file"]
+            seg_path = self.image_folder + d["seg"]
+            document.append([img_path, caption, d])
+        # use llama-index to construct index
+        Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+        image_nodes = [ImageNode(image_path=p, text=t, meta_data=k) for p, t, k in document]
+        self.multi_index = MultiModalVectorStoreIndex(image_nodes, show_progress=True)
+        
+        
+        
+
+
+    def inference(self):
+        set_seed(0)
+        disable_torch_init()
+        with open(args.question_file, 'r', encoding='utf-8') as file:
+            questions = json.load(file)
+        # questions = [json.loads(q) for q in open(args.question_file,"r")]
+        # do inference
+        for line in tqdm(questions):
+            qs = line["text"].replace(DEFAULT_IMAGE_TOKEN, '').strip()
+            image_file = line["image"]
+            if self.model.config.mm_use_im_start_end:
+                qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+                
+            conv = conv_templates[args.conv_mode].copy()
+            conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
+            prompt = conv.get_prompt()
             
-        conv = conv_templates[args.conv_mode].copy()
-        conv.append_message(conv.roles[0], qs)
-        conv.append_message(conv.roles[1], None)
-        prompt = conv.get_prompt()
-        
-        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+            input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
 
-        image = Image.open(os.path.join(args.image_folder, image_file))
-        image_tensor = process_images([image], image_processor, model.config)[0]
-        
-        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
-        keywords = [stop_str]
-        stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
-        
-        with torch.inference_mode():
-            output_ids = model.generate(
-                input_ids,
-                images=image_tensor.unsqueeze(0).half().cuda(),
-                do_sample=True if args.temperature > 0 else False,
-                temperature=args.temperature,
-                top_p=args.top_p,
-                num_beams=args.num_beams,
-                # no_repeat_ngram_size=3,
-                max_new_tokens=1024,
-                use_cache=True)
+            image = Image.open(os.path.join(args.image_folder, image_file))
+            image_tensor = process_images([image], self.image_processor, self.model.config)[0]
+            
+            stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+            keywords = [stop_str]
+            stopping_criteria = KeywordsStoppingCriteria(keywords, self.tokenizer, input_ids)
+            
+            with torch.inference_mode():
+                output_ids = self.model.generate(
+                    input_ids,
+                    images=image_tensor.unsqueeze(0).half().cuda(),
+                    do_sample=True if args.temperature > 0 else False,
+                    temperature=args.temperature,
+                    top_p=args.top_p,
+                    num_beams=args.num_beams,
+                    # no_repeat_ngram_size=3,
+                    max_new_tokens=1024,
+                    use_cache=True)
 
-        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
-        return outputs
-        # TODO:批量测试需要记录上下文信息
+            outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+            return outputs
+            # TODO:批量测试需要记录上下文信息
         
 
 if __name__ == "__main__":
@@ -82,4 +114,6 @@ if __name__ == "__main__":
     parser.add_argument("--num_beams", type=int, default=1)
     args = parser.parse_args()
     
-    print(inference(args))
+    vrag = VRAG(args)
+    # print(vrag.inference())
+    vrag.build_index()
