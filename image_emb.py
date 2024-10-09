18a94b234826479520e75416e7a83626608c313f
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-09 11:32:46 +0800

    extract leasion

--- image_emb.py
+++ image_emb.py
@@ -6,9 +6,13 @@ import numpy as np
 import argparse
 
 class ImgEmb():
-    def __init__(self, device):
+    def __init__(self, color_map, device):
         self.model, self.preprocess = clip.load("ViT-B/32", device=device)
         self.device = device
+        # if os.path.exists(color_map):
+        with open(color_map, "r") as cf:
+            self.color_map = json.load(cf)
+        
     
     def get_features_from_image_path(self, image_paths):
         images = [self.preprocess(Image.open(image_path).convert("RGB")) for image_path in image_paths]
@@ -35,36 +39,7 @@ class ImgEmb():
                 unique_colors.append(color)
 
         return np.array(unique_colors)
-        
-    def open_images(self, image_path, segmentation_path):
-        """打开眼底图和分割图并转换为RGB格式。"""
-        image = Image.open(image_path).convert('RGB')
-        segmentation = Image.open(segmentation_path).convert('RGB')
-        return np.array(image), np.array(segmentation)
-
-    def create_mask(self, segmentation_data, target_color, tolerance):
-        """根据目标颜色和容差创建掩膜。"""
-        lower_bound = np.array(target_color) - tolerance
-        upper_bound = np.array(target_color) + tolerance
-        mask = np.all((segmentation_data >= lower_bound) & (segmentation_data <= upper_bound), axis=-1)
-        return mask
-
-    def crop_and_save_lesions(self, image_data, segmentation_data, color_map, tolerance, output_dir):
-        """根据分割图裁剪并保存病灶。"""
-        os.makedirs(output_dir, exist_ok=True)
-
-        for color, label in color_map.items():
-            mask = self.create_mask(segmentation_data, color, tolerance)
-
-            if np.any(mask):
-                coords = np.argwhere(mask)
-                y0, x0 = coords.min(axis=0)
-                y1, x1 = coords.max(axis=0) + 1
-
-                lesion_crop = image_data[y0:y1, x0:x1]
-                cropped_image = Image.fromarray(lesion_crop)
-                cropped_image.save(os.path.join(output_dir, f'{label}.png'))
-                
+    
     def create_color_images(self, colors, output_dir='output_colors', json_file='color_mapping.json'):
         # 创建输出目录
         if not os.path.exists(output_dir):
@@ -87,18 +62,65 @@ class ImgEmb():
         # 保存到JSON文件
         with open(json_file, 'w') as f:
             json.dump(color_mapping, f, indent=4)
+        
+    def extract_lesions(self, image_path, mask_path, output_dir='lesion_images', json_file='lesion_mapping.json', delta=100):
+         # 创建输出目录
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # 打开原图和分割图
+        p = Image.open(image_path).convert('RGB')
+        mask = Image.open(mask_path).convert('RGB')
+
+        lesion_mapping = {}
+
+        for lesion_name, color in self.color_map.items():
+            # 创建掩模
+            mask_array = np.array(mask)
+            p_array = np.array(p)
+            mask_mask = np.zeros(mask_array.shape[:2], dtype=bool)
+
+            # 检查每个像素的颜色距离
+            for i in range(mask_array.shape[0]):
+                for j in range(mask_array.shape[1]):
+                    if self.color_distance(mask_array[i, j], color) < delta:
+                        mask_mask[i, j] = True
+
+            # 提取病灶区域
+            lesion_image = np.zeros_like(p_array)
+            lesion_image[mask_mask] = p_array[mask_mask]
+
+            # 创建病灶图像并保存
+            lesion_img = Image.fromarray(lesion_image)
+            file_name = f"{lesion_name}.png"
+            lesion_img.save(f"{output_dir}/{file_name}")
+
+            # 存储病灶名称和文件名的映射
+            lesion_mapping[lesion_name] = file_name
+
+        # 保存到JSON文件
+        with open(json_file, 'w') as f:
+            json.dump(lesion_mapping, f, indent=4)
+                
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--img-path", type=str, default="./segmentation/")
     parser.add_argument("--color-path", type=str, default="./segmentation/color.jpg")    
+    parser.add_argument("--color-map", type=str, default="./data/color.json")
     args = parser.parse_args()
     image_paths = [os.path.join(args.img_path,file) for file in os.listdir(args.img_path)]
-    IE = ImgEmb(args.device)
-    colors = IE.get_lesion_colors(args.color_path)
-    IE.create_color_images(colors, output_dir="./data/output_colors", json_file="./data/color_mapping.json")
+    IE = ImgEmb(args.color_map, args.device)
     
+    # 获取病灶颜色对应关系
+    # colors = IE.get_lesion_colors(args.color_path)
+    # IE.create_color_images(colors, output_dir="./data/output_colors", json_file="./data/color_mapping.json")
+    
+    # 根据seg提取病灶crop
+    mask_path = "./segmentation/007-6008-300.jpg"
+    img_path = "./segmentation/007-6008-300.png"
+    IE.extract_lesions(img_path ,mask_path, output_dir="./data/lesion/007-6008-300/", json_file="./data/lesion/lesion_map_007-6008-300.json")
     # print(image_paths)
     # feature = IE.get_features_from_image_path(image_paths)
     # print(feature)
