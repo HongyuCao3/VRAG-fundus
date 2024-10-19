dff11343ca3e4714e113581a290878d5c4aaad37
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-19 20:22:51 +0800

    add script for emb

--- scripts/build index/build index.sh
+++ scripts/build index/build index.sh
@@ -1,6 +1,15 @@
+# build level
+/home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python index_builder.py \
+    --level-dir "./data/level/" \
+    --persist-dir "./data/level_emb/" 
+
+
+# build all
 /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
 python index_builder.py \
     --crop-dir "./data/lesion/" \
     --level-dir "./data/level/" \
-    --persist-dir "./data/level_emb/" \
+    --persist-dir "./data/level_crop_emb/" 
