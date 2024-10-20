9e4c3920f4b9f0c75b3a4155350f5b5a33f69165
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-20 15:40:31 +0800

    add rop test

--- scripts/evaluation/evaluate_dr.sh
+++ scripts/evaluation/evaluate_dr.sh
@@ -59,7 +59,7 @@ python evaluation.py \
     --use-rag True \
     --test-num 5
 
-# emb-level rag llava-med
+# crop-emb rag llava-med
 crop_emb=emb_crop
 level_emb=level_emb
 crop_emb_path="./data/${crop_emb}"
@@ -67,14 +67,13 @@ level_emb_path="./data/${level_emb}"
 dataset=DR
 m=1
 n=1
-output_path="./output/${dataset}/${crop_emb}_${level_emb}_${m}_${n}_rag.json"
+output_path="./output/${dataset}/${crop_emb}_${m}_${n}_rag.json"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
 python evaluation.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --crop-emb-path ${crop_emb_path} \
-    --level-emb-path ${level_emb_path} \
     --chunk-m ${m} \
     --chunk-n ${n} \
     --use-rag True \
