3d65ef46dfa65078dbe7a1209771548910af8619
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-23 15:45:06 +0800

    add raw analysis script

--- scripts/analysis/dr_analysis.sh
+++ scripts/analysis/dr_analysis.sh
@@ -15,3 +15,22 @@ conda activate llava-med
 python analysis.py \
     --file-path ${output_path} \
     --res-path ${res_path}
+
+
+# raw finetuned analysis
+crop_emb=emb_crop
+level_emb=level_emb
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${crop_emb}_${level_emb}_${m}_${n}_${test_num}.json"
+res_path="./output/${dataset}/llava-med-finetuned/analysis/raw_${m}_${n}_${tet_num}_pics.txt"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis.py \
+    --file-path ${output_path} \
+    --res-path ${res_path}
