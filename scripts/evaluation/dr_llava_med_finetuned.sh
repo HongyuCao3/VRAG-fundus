b4b21e7b31165374575f38d66a3be0c54695de62
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-25 10:18:51 +0800

    add script for rotation kb

--- scripts/evaluation/dr_llava_med_finetuned.sh
+++ scripts/evaluation/dr_llava_med_finetuned.sh
@@ -131,6 +131,32 @@ nohup python evaluation.py \
     --test-num ${test_num} \
     >${log_path} 2>&1 &
 
+# level rotation rag llava-med finetuned with pics
+crop_emb=emb_crop
+level_emb=level_emb_r
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${level_emb}_${m}_${n}_rag_${test_num}_pics.json"
+log_path="./output/${dataset}/llava-med-finetuned/log/${level_emb}_${m}_${n}_rag_${tet_num}_pics.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --level-emb-path ${level_emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    >${log_path} 2>&1 &
+
 
 # classic level rag llava-med finetuned with pics
 crop_emb=emb_crop
