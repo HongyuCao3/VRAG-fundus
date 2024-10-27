6313a8eb2ed59c043a801bbe1b7a5f26d9fa196b
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-10-27 09:46:13 +0800

    add script for crop chunk analysis

--- scripts/analysis/dr_analysis.sh
+++ scripts/analysis/dr_analysis.sh
@@ -90,3 +90,21 @@ conda activate llava-med
 python analysis.py \
     --file-path ${output_path} \
     --res-path ${res_path}
+
+# emb finetuned analysis
+crop_emb=emb_crop
+level_emb=classic_dr_emb
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=2
+n=2
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${crop_emb}_${m}_${n}_rag_${test_num}_pics.json"
+res_path="./output/${dataset}/llava-med-finetuned/analysis/${crop_emb}_${m}_${n}_rag_${test_num}_pics.txt"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis.py \
+    --file-path ${output_path} \
+    --res-path ${res_path}
