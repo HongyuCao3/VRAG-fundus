c019fab7129abc84e441688086a77809c55f32a3
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-03 16:09:42 +0800

    add layer=3

--- scripts/build emb/level.sh
+++ scripts/build emb/level.sh
@@ -8,3 +8,9 @@ python emb_builder.py \
     --img-path "./data/level/" \
     --emb-path "./data/level_emb_clip_${layer}/" \
     --layer ${layer}
+
+layer=3
+python emb_builder.py \
+    --img-path "./data/level/" \
+    --emb-path "./data/level_emb_clip_${layer}/" \
+    --layer ${layer}
