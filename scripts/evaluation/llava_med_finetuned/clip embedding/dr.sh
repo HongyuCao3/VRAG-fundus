42d01add518d741f9812872845dfe69ea5f7e581
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-05 10:35:26 +0800

    add script for crop ablation

--- scripts/evaluation/llava_med_finetuned/clip embedding/dr.sh
+++ scripts/evaluation/llava_med_finetuned/clip embedding/dr.sh
@@ -133,3 +133,55 @@ nohup python evaluation.py \
     --test-num ${test_num} \
     --layer 3 \
     >${log_path} 2>&1 &
+
+# crop 6
+crop_emb=crop_emb_clip_6
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${crop_emb}_${m}_${n}_rag_${test_num}_pics.json"
+log_path="./output/${dataset}/llava-med-finetuned/log/${crop_emb}_${m}_${n}_rag_${tet_num}_pics.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --crop-emb-path ${crop_emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    >${log_path} 2>&1 &
+
+# crop 3
+crop_emb=crop_emb_clip_3
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${crop_emb}_${m}_${n}_rag_${test_num}_pics.json"
+log_path="./output/${dataset}/llava-med-finetuned/log/${crop_emb}_${m}_${n}_rag_${tet_num}_pics.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --crop-emb-path ${crop_emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    >${log_path} 2>&1 &
