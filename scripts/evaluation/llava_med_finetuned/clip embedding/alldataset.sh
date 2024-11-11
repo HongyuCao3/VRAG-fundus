998826886b280644451910a736f908e51c01574e
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-11 15:57:46 +0800

    add script for all classic

---
+++ scripts/evaluation/llava_med_finetuned/clip embedding/alldataset.sh
@@ -0,0 +1,23 @@
+classic_emb=classic_emb_clip
+dataset=ALL
+classic_emb_path="./data/${classic_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/llava-med-finetuned/${classic_emb}_${m}_${n}_rag_${test_num}_pics.json"
+log_path="./output/${dataset}/llava-med-finetuned/log/${classic_emb}_${m}_${n}_rag_${tet_num}_pics.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --classic-emb-path ${classic_emb_path} \
+    --conv-mode mistral_instruct \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    >${log_path} 2>&1 &
