09107b0f63bbd9b9af8d0a40c38641df22e78888
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-12 10:24:14 +0800

    add scropt

--- scripts/analysis/internVL2/clip_embedding/dr.sh
+++ scripts/analysis/internVL2/clip_embedding/dr.sh
@@ -17,7 +17,64 @@ python analysis_multi_turn.py \
     --res-path ${res_path} \
     --level-emb ${level_emb_path}
 
-# crop & level
+# crop
+crop_emb=crop_emb_clip
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/internVL2/${crop_emb}_${m}_${n}_rag_${test_num}_pics.json"
+res_path="./output/${dataset}/internVL2/analysis/${crop_emb}_${m}_${n}_rag_${test_num}_pics.png"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis_multi_turn.py \
+    --file-path ${output_path} \
+    --res-path ${res_path} \
+    --level-emb ${level_emb_path}
+
+#level
+crop_emb=crop_emb_clip
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/internVL2/${level_emb}_${m}_${n}_rag_${test_num}_pics.json"
+res_path="./output/${dataset}/internVL2/analysis/${level_emb}_${m}_${n}_rag_${test_num}_pics.png"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis_multi_turn.py \
+    --file-path ${output_path} \
+    --res-path ${res_path} \
+    --level-emb ${level_emb_path}
+
+#crop & level
+crop_emb=crop_emb_clip
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+output_path="./output/${dataset}/internVL2/${level_emb}_${crop_emb}_${m}_${n}_rag_${test_num}_pics.json"
+res_path="./output/${dataset}/internVL2/analysis/${level_emb}_${crop_emb}_${m}_${n}_rag_${test_num}_pics.png"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis_multi_turn.py \
+    --file-path ${output_path} \
+    --res-path ${res_path} \
+    --level-emb ${level_emb_path}
+
+# crop & level mulit turn
 crop_emb=crop_emb_clip
 level_emb=level_emb_clip
 dataset=DR
