061e2790315dde448c1c56d823e5ef194f55b107
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-12 10:17:28 +0800

    add chekc analysis script

--- scripts/analysis/internVL2/clip_embedding/dr.sh
+++ scripts/analysis/internVL2/clip_embedding/dr.sh
@@ -37,3 +37,25 @@ python analysis_multi_turn.py \
     --res-path ${res_path} \
     --level-emb ${level_emb_path} \
     --step ${step}
+
+# crop & level check
+crop_emb=crop_emb_clip
+level_emb=level_emb_clip
+dataset=DR
+crop_emb_path="./data/${crop_emb}"
+level_emb_path="./data/${level_emb}"
+m=1
+n=1
+test_num=-1
+step=3
+mode=MultiTurnCheck
+output_path="./output/${dataset}/internVL2/${level_emb}_${crop_emb}_${m}_${n}_rag_multiturn_${test_num}_${mode}.json"
+res_path="./output/${dataset}/internVL2/analysis/${level_emb}_${crop_emb}_${m}_${n}_rag_multiturn_${test_num}_${mode}_${step}.png"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+python analysis_multi_turn.py \
+    --file-path ${output_path} \
+    --res-path ${res_path} \
+    --level-emb ${level_emb_path} \
+    --step ${step}
