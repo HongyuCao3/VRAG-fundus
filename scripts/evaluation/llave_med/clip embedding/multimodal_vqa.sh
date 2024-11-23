e30b29212a534e46ffb8032c9734ad48f92e348e
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-11-23 15:39:46 +0800

    add scropt for classci emb multi modal evaluation

--- scripts/evaluation/llave_med/clip embedding/multimodal_vqa.sh
+++ scripts/evaluation/llave_med/clip embedding/multimodal_vqa.sh
@@ -6,7 +6,7 @@ dataset=MultiModal
 classic_emb_path="./data/${classic_emb}"
 m=1
 n=1
-test_num=5
+test_num=-1
 output_path="./output/${dataset}/llava-med/raw_${m}_${n}_rag_${test_num}.json"
 log_path="./output/${dataset}/llava-med/log/raw_${m}_${n}_rag_${test_num}.log"
 model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/llava-med-v1.5-mistral-7b"
@@ -16,6 +16,32 @@ nohup python evaluation.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    --mode ALL \
+    >${log_path} 2>&1 &
+
+
+# classic emb
+classic_emb=classic_emb_clip
+dataset=MultiModal
+classic_emb_path="./data/${classic_emb}"
+m=1
+n=1
+test_num=5
+output_path="./output/${dataset}/llava-med/${classic_emb}_${m}_${n}_rag_${test_num}_pics.json"
+log_path="./output/${dataset}/llava-med/log/${classic_emb}_${m}_${n}_rag_${test_num}_pics.log"
+model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/llava-med-v1.5-mistral-7b"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --classic-emb-path ${classic_emb_path} \
     --chunk-m ${m} \
     --chunk-n ${n} \
     --use-rag True \
