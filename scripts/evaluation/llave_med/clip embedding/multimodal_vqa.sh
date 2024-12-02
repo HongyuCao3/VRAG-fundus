a55862c58726531b53a10ba58d49ff93bf2e1484
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-02 16:16:31 +0800

    change args

--- scripts/evaluation/llave_med/clip embedding/multimodal_vqa.sh
+++ scripts/evaluation/llave_med/clip embedding/multimodal_vqa.sh
@@ -7,12 +7,12 @@ classic_emb_path="./data/${classic_emb}"
 m=1
 n=1
 test_num=-1
-output_path="./output/${dataset}/llava-med/raw_${m}_${n}_rag_${test_num}.json"
-log_path="./output/${dataset}/llava-med/log/raw_${m}_${n}_rag_${test_num}.log"
+output_path="./output/${dataset}/llava-med/raw_${m}_${n}_rag_${test_num}3.json"
+log_path="./output/${dataset}/llava-med/log/raw_${m}_${n}_rag_${test_num}3.log"
 model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/llava-med-v1.5-mistral-7b"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
-nohup python evaluation.py \
+nohup python evaluation_llava.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
@@ -32,12 +32,12 @@ classic_emb_path="./data/${classic_emb}"
 m=1
 n=1
 test_num=-1
-output_path="./output/${dataset}/llava-med/${classic_emb}_${m}_${n}_rag_${test_num}_pics.json"
-log_path="./output/${dataset}/llava-med/log/${classic_emb}_${m}_${n}_rag_${test_num}_pics.log"
+output_path="./output/${dataset}/llava-med/${classic_emb}_${m}_${n}_rag_${test_num}_pics3.json"
+log_path="./output/${dataset}/llava-med/log/${classic_emb}_${m}_${n}_rag_${test_num}_pics3.log"
 model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/llava-med-v1.5-mistral-7b"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
-nohup python evaluation.py \
+nohup python evaluation_llava.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
