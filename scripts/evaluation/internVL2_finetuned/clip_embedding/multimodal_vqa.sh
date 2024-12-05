ac57704a6904dcf024f4cedf3811677e7c2bf84b
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-05 10:25:54 +0800

    change args

--- scripts/evaluation/internVL2_finetuned/clip_embedding/multimodal_vqa.sh
+++ scripts/evaluation/internVL2_finetuned/clip_embedding/multimodal_vqa.sh
@@ -4,12 +4,13 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 classic_emb=classic_emb_clip
 dataset=MultiModal
 classic_emb_path="./data/${classic_emb}"
+model_name="internVL2_finetuned"
 m=1
 n=1
 test_num=-1
-output_path="./output/${dataset}/internVL2/raw_${m}_${n}_rag_${test_num}_pics3.json"
-log_path="./output/${dataset}/internVL2/log/raw_${m}_${n}_rag_${test_num}_pics3.log"
-model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/InternVL2-8B"
+output_path="./output/${dataset}/${model_name}/raw_${m}_${n}_rag_${test_num}_pics3.json"
+log_path="./output/${dataset}/${model_name}/log/raw_${m}_${n}_rag_${test_num}_pics3.log"
+model_path="/home/louw/InternVL/internvl_chat/work_dirs/internvl_chat_v2_0/internvl2_8b_internlm2_7b_dynamic_res_2nd_finetune_lora_fulldataset"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
 nohup python evaluation.py \
