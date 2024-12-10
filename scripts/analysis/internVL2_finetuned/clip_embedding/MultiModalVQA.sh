97c6b87dca0bce81cdb96973b89584acc930940d
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-10 10:07:14 +0800

    add script for filter

--- scripts/analysis/internVL2_finetuned/clip_embedding/MultiModalVQA.sh
+++ scripts/analysis/internVL2_finetuned/clip_embedding/MultiModalVQA.sh
@@ -32,3 +32,23 @@ conda activate internvl_louwei
 python analysis_.py \
     --file-path ${output_path} \
     --res-path ${res_path} 
+
+# filter
+classic_emb=classic_emb_mvqa
+dataset=MultiModal
+classic_emb_path="./data/${classic_emb}"
+m=1
+n=1
+test_num=-1
+model=internVL2_finetuned
+filter=True
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_${filter}_pics
+output_path="./output/${dataset}/${model}/${save_tmp}.json"
+log_path="./output/${dataset}/${model}/log/${save_tmp}.log"
+res_path="./output/${dataset}/${model}/analysis/${save_tmp}.png"
+model_path="/home/hongyu/Visual-RAG-LLaVA-Med/Model/InternVL2-8B"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate internvl_louwei
+python analysis_.py \
+    --file-path ${output_path} \
+    --res-path ${res_path} 
--- scripts/evaluation/internVL2_finetuned/clip_embedding/multimodal_vqa.sh
+++ scripts/evaluation/internVL2_finetuned/clip_embedding/multimodal_vqa.sh
@@ -52,3 +52,31 @@ nohup python evaluation.py \
     --test-num ${test_num} \
     --mode ALL \
     >${log_path} 2>&1 &
+
+# filter
+classic_emb=classic_emb_mvqa
+dataset=MultiModal
+classic_emb_path="./data/${classic_emb}"
+m=1
+n=1
+test_num=-1
+filter=True
+model_name="internVL2_finetuned"
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_${filter}_pics
+output_path="./output/${dataset}/${model_name}/${save_tmp}.json"
+log_path="./output/${dataset}/${model_name}/log/${save_tmp}.log"
+model_path="/home/hongyu/InternVL/internvl2_8b_internlm2_7b_dynamic_res_2nd_finetune_lora_fulldataset"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate internvl_louwei
+nohup python evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --classic-emb-path ${classic_emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    --mode ALL \
+    >${log_path} 2>&1 &
