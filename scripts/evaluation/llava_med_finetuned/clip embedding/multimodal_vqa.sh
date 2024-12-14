590aff79e18fbdcf4ca09917597e5192fe393e8b
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-14 20:42:50 +0800

    change args

--- scripts/evaluation/llava_med_finetuned/clip embedding/multimodal_vqa.sh
+++ scripts/evaluation/llava_med_finetuned/clip embedding/multimodal_vqa.sh
@@ -58,8 +58,12 @@ classic_emb_path="./data/${classic_emb}"
 m=1
 n=1
 test_num=-1
-output_path="./output/${dataset}/llava-med-finetuned/${classic_emb}_${m}_${n}_rag_${test_num}_pics3.json"
-log_path="./output/${dataset}/llava-med-finetuned/log/${classic_emb}_${m}_${n}_rag_${test_num}_pics3.log"
+t_filter=0.5
+t_check=0.5
+model_name=llava-med-finetuned
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_2d_mh
+output_path="./output/${dataset}/${model_name}/${save_tmp).json"
+log_path="./output/${dataset}/${model_name}/log/${save_tmp}.log"
 model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate llava-med
