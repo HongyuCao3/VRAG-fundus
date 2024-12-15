9f3ece3bc2e39c7991044108148a2cf48f2b2c9b
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-15 20:51:55 +0800

    build oct emb

--- KnowledgeBase/shell/classic.sh
+++ KnowledgeBase/shell/classic.sh
@@ -10,3 +10,11 @@ emb_path=${cur_path}"/KnowledgeBase/emb_savings/classic_emb_ffa"
 python ./KnowledgeBase/emb_builder.py \
     --img-path ${img_dir} \
     --emb-path ${emb_path}
+
+# OCT emb
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+img_dir=${cur_path}"/KnowledgeBase/img_savings/Classic_Images_(OCT)"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/classic_emb_oct"
+python ./KnowledgeBase/emb_builder.py \
+    --img-path ${img_dir} \
+    --emb-path ${emb_path}
