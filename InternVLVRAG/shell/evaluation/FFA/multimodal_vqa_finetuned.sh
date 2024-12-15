8d5ea686f300859788059c23412a394d24b3761a
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-15 19:58:18 +0800

    change args

--- InternVLVRAG/shell/evaluation/FFA/multimodal_vqa_finetuned.sh
+++ InternVLVRAG/shell/evaluation/FFA/multimodal_vqa_finetuned.sh
@@ -50,7 +50,7 @@ log_path=${cur_path}"/InternVLVRAG/output/${dataset}/${model_name}/log/${save_tm
 model_path="/home/hongyu/InternVL/internvl2_8b_internlm2_7b_dynamic_res_2nd_finetune_lora_fulldataset"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate internvl_louwei
-nohup python evaluation.py \
+nohup python ./InternVLVRAG/evaluation.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
@@ -64,21 +64,24 @@ nohup python evaluation.py \
     >${log_path} 2>&1 &
 
 # filter
-classic_emb=classic_emb_mvqa
+classic_emb=classic_emb_ffa
 dataset=MultiModal
-classic_emb_path="./data/${classic_emb}"
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="finetuned"
+sheet_names="FFA"
 m=1
 n=1
 test_num=-1
-filter=True
-model_name="internVL2_finetuned"
-save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_${filter}_pics_2d
-output_path="./output/${dataset}/${model_name}/${save_tmp}.json"
-log_path="./output/${dataset}/${model_name}/log/${save_tmp}.log"
+t_check=-1
+t_filter=0.5
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/InternVLVRAG/output/${dataset}/${model_name}/${save_tmp}.json"
+log_path=${cur_path}"/InternVLVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
 model_path="/home/hongyu/InternVL/internvl2_8b_internlm2_7b_dynamic_res_2nd_finetune_lora_fulldataset"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate internvl_louwei
-nohup python evaluation.py \
+nohup python ./InternVLVRAG/evaluation.py \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
@@ -93,24 +96,24 @@ nohup python evaluation.py \
     >${log_path} 2>&1 &
 
 # filter+check
-classic_emb=classic_emb_mvqa
+classic_emb=classic_emb_ffa
 dataset=MultiModal
-classic_emb_path="./data/${classic_emb}"
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="finetuned"
+sheet_names="FFA"
 m=1
 n=1
 test_num=-1
-filter=True
-check=True
-t_filter=0.5
 t_check=0.5
-model_name="internVL2_finetuned"
-save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_2d_mh
-output_path="./output/${dataset}/${model_name}/${save_tmp}.json"
-log_path="./output/${dataset}/${model_name}/log/${save_tmp}.log"
+t_filter=0.5
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/InternVLVRAG/output/${dataset}/${model_name}/${save_tmp}.json"
+log_path=${cur_path}"/InternVLVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
 model_path="/home/hongyu/InternVL/internvl2_8b_internlm2_7b_dynamic_res_2nd_finetune_lora_fulldataset"
 cd /home/hongyu/Visual-RAG-LLaVA-Med
 conda activate internvl_louwei
-nohup python evaluation.py \
+nohup python ./InternVLVRAG/evaluation.py  \
     --dataset ${dataset} \
     --output-path ${output_path} \
     --model-path ${model_path} \
