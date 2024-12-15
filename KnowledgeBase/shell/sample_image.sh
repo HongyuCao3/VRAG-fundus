4c91ab065523c5124d78aaf82cae9c07616d7165
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-15 20:48:58 +0800

    add class for oct

--- KnowledgeBase/shell/sample_image.sh
+++ KnowledgeBase/shell/sample_image.sh
@@ -14,4 +14,4 @@ cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
 tgt_dir=${cur_path}"/KnowledgeBase/img_savings/Classic\Images\(OCT)"
 python ./KnowledgeBase/image_samplar.py \
     --tgt-dir ${tgt_dir} \
-    --diag-list "diabetic retinopathy" "wet age-related macular degeneration" "dry age-related macular degeneration" central retinal vein occlusion" branch retinal vein occlusion" "central serous chorioretinopathy" "choroidal melanoma" "Coats Disease" "familial exudative vitreoretinopathy" "Vogt-Koyanagi-Harada disease"
+    --diag-list "cystoid macular edema" "central serous chorioretinopathy" "dry age-related macular degeneration" "epiretinal membrane" "macular hole" "polypoidal choroidal vasculopathy" "retinal detachment" "retinoschisis" "retinal vein occlusion" "wet age-related macular degeneration"
