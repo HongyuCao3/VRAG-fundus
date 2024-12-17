c89216bfe30fb7a715ee3f6680bafbfd9554390c
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-17 09:53:07 +0800

    add main

---
+++ AnalysisVRAG/main.py
@@ -0,0 +1,18 @@
+import os
+import json
+import argparse
+from AnalysisVRAG.mulit_modal_vqa_analysis import MultiVQAAnalysis
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file-path", type=str, default="")
+    parser.add_argument("--res-path", type=str, default="")
+    parser.add_argument("--level-emb", type=bool, default=False)
+    parser.add_argument("--num", type=int, default=-1)
+    parser.add_argument("--step", type=int, default=4)
+    parser.add_argument("--sheet-names", type=str, nargs='+', default=["CFP"])
+    args = parser.parse_args()
+    analysis = MultiVQAAnalysis(args.file_path)
+    cm = analysis.calculate_confusion_matrix(args.res_path)
+    print(f"Accuracy: {analysis.calculate_accuracy():.4f}")
+    print(cm)
