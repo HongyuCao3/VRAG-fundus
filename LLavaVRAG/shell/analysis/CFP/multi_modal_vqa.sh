e6574e5c3d2d2feb8ebe14c0bfacec8005579cc9
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-26 10:21:34 +0800

    add script for cfp

---
+++ LLavaVRAG/shell/analysis/CFP/multi_modal_vqa.sh
@@ -0,0 +1,21 @@
+# raw
+# raw
+classic_emb=classic_emb_cfp
+dataset=MultiModalVQA
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="llava-med"
+sheet_names="CFP"
+m=1
+n=1
+test_num=-1
+save_tmp=raw_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/${save_tmp}.csv"
+log_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
+save_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/analysis/${save_tmp}.json"
+map_path="./data/mapping_relationship.xlsx"
+conda activate internvl_louwei
+python ./LLavaVRAG/analysis.py \
+    --res-path ${output_path} \
+    --map-path ${map_path} \
+    --save-path ${save_path}
