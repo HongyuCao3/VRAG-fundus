e4b808e225513802784587def1cbb20bb966923a
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-27 13:18:00 +0800

    change args

---
+++ LLavaVRAG/shell/evaluation/FFA/multi_modal_vqa_finetuned.sh
@@ -0,0 +1,100 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+# raw
+classic_emb=classic_emb_ffa
+dataset=MultiModalVQA
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="finetuned"
+sheet_names="FFA"
+m=1
+n=1
+test_num=-1
+t_filter=-1
+t_check=-1
+save_tmp=raw_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/${save_tmp}.csv"
+log_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python ./LLavaVRAG/evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --conv-mode mistral_instruct \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    --mode ALL \
+    --sheet-names ${sheet_names} \
+    >${log_path} 2>&1 &
+
+# classic
+classic_emb=classic_emb_ffa
+dataset=MultiModalVQA
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="finetuned"
+sheet_names="FFA"
+m=1
+n=1
+test_num=-1
+t_filter=-1
+t_check=-1
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/${save_tmp}.csv"
+log_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python ./LLavaVRAG/evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --conv-mode mistral_instruct \
+    --classic-emb-path ${emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    --mode ALL \
+    --sheet-names ${sheet_names} \
+    >${log_path} 2>&1 &
+
+# filter
+classic_emb=classic_emb_ffa
+dataset=MultiModalVQA
+cur_path="/home/hongyu/Visual-RAG-LLaVA-Med"
+emb_path=${cur_path}"/KnowledgeBase/emb_savings/"${classic_emb}
+model_name="finetuned"
+sheet_names="FFA"
+m=1
+n=1
+test_num=-1
+t_filter=0.7
+save_tmp=${classic_emb}_${m}_${n}_rag_${test_num}_filter_${t_filter}_check_${t_check}_modality_${sheet_names}
+output_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/${save_tmp}.csv"
+log_path=${cur_path}"/LLavaVRAG/output/${dataset}/${model_name}/log/${save_tmp}.log"
+model_path="/home/hongyu/eye_llava_medllava_finetune_mistral"
+cd /home/hongyu/Visual-RAG-LLaVA-Med
+conda activate llava-med
+nohup python ./LLavaVRAG/evaluation.py \
+    --dataset ${dataset} \
+    --output-path ${output_path} \
+    --model-path ${model_path} \
+    --conv-mode mistral_instruct \
+    --classic-emb-path ${emb_path} \
+    --chunk-m ${m} \
+    --chunk-n ${n} \
+    --use-rag True \
+    --use-pics True \
+    --test-num ${test_num} \
+    --mode ALL \
+    --filter \
+    --t-filter ${t_filter} \
+    --sheet-names ${sheet_names} \
+    >${log_path} 2>&1 &
