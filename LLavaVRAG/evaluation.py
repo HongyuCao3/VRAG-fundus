e4501385ca14b188fb920c20b628516b1fc598a7
HongyuCao <pb18111726@mail.ustc.edu.cn>
2024-12-28 16:11:22 +0800

    change test type

--- LLavaVRAG/evaluation.py
+++ LLavaVRAG/evaluation.py
@@ -214,5 +214,7 @@ if __name__ == "__main__":
     vrag = VRAG(args) # llava, llava-med, llava-med-rag
     # vrag = InternVL2(args)
     eva = evaluation(args, vrag)
-    # eva.test2()
-    eva.test_lesion_balanced()
+    if args.dataset == "MultiModalVQA":
+        eva.test2()
+    elif args.dataset == "LesionBalanced":
+        eva.test_lesion_balanced()
