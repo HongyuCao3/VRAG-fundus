ddd30dcf93578b3089bd2875d274cbeb0614f390
HongyuCao <pb18111726@mail.ustc.edu.cn>
2025-02-02 14:30:49 +0800

    can inference

--- QwenVLVRAG/QwenVL_vrag.py
+++ QwenVLVRAG/QwenVL_vrag.py
@@ -1,13 +1,24 @@
 from transformers import AutoProcessor, Qwen2_5_VLForConditionalGeneration
+import torch
+import pathlib
+import sys
+
+sys.path.append(str(pathlib.Path.cwd()))
+from Datasets.MultiModalClassificationDataset import (
+    MultiModalClassificationDataset,
+    MultiModalClassificationConfig,
+)
 from qwen_vl_utils import process_vision_info
 
 
 class QwenVLVRAG:
-    def __init__(self, checkpoint_path="Qwen/Qwen2.5-VL-7B-Instruct"):
+    def __init__(self, checkpoint_path="./Model/Qwen2.5-VL-7B-Instruct"):
+        min_pixels = 256 * 28 * 28
+        max_pixels = 1280 * 28 * 28
         self.model =  Qwen2_5_VLForConditionalGeneration.from_pretrained(
-        checkpoint_path, torch_dtype="auto", device_map="auto"
+        checkpoint_path, torch_dtype=torch.bfloat16, device_map="cuda:0",
         )
-        self.processor = AutoProcessor.from_pretrained(checkpoint_path)
+        self.processor = AutoProcessor.from_pretrained(checkpoint_path, min_pixels=min_pixels, max_pixels=max_pixels)
 
     def inference(self, messages):
         text = self.processor.apply_chat_template(
@@ -32,3 +43,23 @@ class QwenVLVRAG:
             generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
         return output_text
+    
+    
+if __name__ == "__main__":
+    qvlvrag = QwenVLVRAG()
+    image_path = "./data/Classic Images/DR/mild NPDR_1.jpeg"
+    query = "What is the diagnsis"
+    messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                            "image": image_path,
+                        },
+                        {"type": "text", "text": query},
+                    ],
+                }
+            ]
+    answer = qvlvrag.inference(messages=messages)
+    print(answer)
