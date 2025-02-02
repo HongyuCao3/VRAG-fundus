ece2f205f337a6b7b7fd1a890e42b13bb88b0bdb
HongyuCao <pb18111726@mail.ustc.edu.cn>
2025-02-02 20:58:57 +0800

    can not use llama-index to retrieve text from image

--- fundus_knowledge_base/index_manager/mulit_disease_index_manager.py
+++ fundus_knowledge_base/index_manager/mulit_disease_index_manager.py
@@ -11,6 +11,7 @@ from fundus_knowledge_base.data_extractor.multi_disease_data_extractor import (
 from llama_index.embeddings.clip import ClipEmbedding
 from llama_index.core.schema import ImageNode, TextNode
 import pathlib
+from llama_index.core.schema import ImageDocument
 
 
 class MulitDiseaseConfig:
@@ -87,7 +88,7 @@ class MultiDiseaseIndexManager(BaseIndexManager):
             multi_index = None
         return multi_index
     
-    def retrieve(self, multi_index, img_path, top_k):
+    def retrieve_image(self, multi_index, img_path, top_k):
         txt = []
         score = [] 
         img = [] 
@@ -100,7 +101,22 @@ class MultiDiseaseIndexManager(BaseIndexManager):
                 score.append(node.get_score()) # 0.628
                 img.append(node.node.image_path)
                 metadata.append(node.node.metadata)
-            # TODO: add text retrieve
+        return {"txt": txt, "score": score, "img": img, "metadata": metadata}
+    
+    def retrieve_text(self, multi_index, img_path, top_k):
+        txt = []
+        score = [] 
+        img = [] 
+        metadata = []
+        if multi_index != None:
+            retrieve_data = multi_index.as_retriever(similarity_top_k=top_k, image_similarity_top_k=top_k)
+            # 这里无法从image retrieve text
+            nodes = retrieve_data.retrieve(image_path)
+            for node in nodes:
+                txt.append(node.get_text()) # excudates
+                score.append(node.get_score()) # 0.628
+                # img.append(node.node.image_path)
+                metadata.append(node.node.metadata)
         return {"txt": txt, "score": score, "img": img, "metadata": metadata}
     
 if __name__ == "__main__":
@@ -108,19 +124,19 @@ if __name__ == "__main__":
     text_file = pathlib.Path("./data/Classic Images/classic.json",)
     text_index_saving_folder = pathlib.Path("./fundus_knowledge_base/emb_savings/mulit_desease_text_index")
     image_index_saving_folder = pathlib.Path("./fundus_knowledge_base/emb_savings/mulit_desease_image_index")
+    image_path = "./data/Classic Images/DR/mild NPDR_1.jpeg"
     
     # build index
     # mdim.build_text_index(text_file="./data/Classic Images/classic.json", saving_folder=text_index_saving_folder)
     # mdim.build_image_index(image_folder=pathlib.Path("./data/Classic Images/"), aving_folder=image_index_saving_folder)
     
     # image retrieve
-    image_index = mdim.load_index(saving_folder=image_index_saving_folder)
-    image_path = "./data/Classic Images/DR/mild NPDR_1.jpeg"
-    result = mdim.retrieve(multi_index=image_index, img_path=image_path, top_k=3)
-    print(result)
+    # image_index = mdim.load_index(saving_folder=image_index_saving_folder)
+    # result = mdim.retrieve(multi_index=image_index, img_path=image_path, top_k=3)
+    # print(result)
     
     # text retrieve
-    # text_index = mdim.load_index(saving_folder=saving_folder)
-    # retrieve_data = text_index.as_retriever(similarity_top_k=1, image_similarity_top_k=3)
-    # node = retrieve_data.retrieve
+    text_index = mdim.load_index(saving_folder=text_index_saving_folder)
+    retrieve_data = mdim.retrieve_text(multi_index=text_index, img_path=image_path, top_k=3)
+    print(retrieve_data)
     
