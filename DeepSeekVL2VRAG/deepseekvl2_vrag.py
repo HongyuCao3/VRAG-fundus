9975b33d2b2060291712076204ec07a347238c6e
HongyuCao <pb18111726@mail.ustc.edu.cn>
2025-02-17 15:40:10 +0800

    can use 768x768

--- DeepSeekVL2VRAG/deepseekvl2_vrag.py
+++ DeepSeekVL2VRAG/deepseekvl2_vrag.py
@@ -3,13 +3,36 @@ import sys, pathlib
 sys.path.append("/home/hongyu/Visual-RAG-LLaVA-Med")
 
 import torch
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoProcessor
 
 from DeepSeekVL2VRAG.DeepSeek_VL2.deepseek_vl2.models import (
     DeepseekVLV2Processor,
     DeepseekVLV2ForCausalLM,
 )
 from DeepSeekVL2VRAG.DeepSeek_VL2.deepseek_vl2.utils.io import load_pil_images
+
+from PIL import Image
+
+def resize_images(images: list[Image.Image], size: tuple) -> list[Image.Image]:
+    """
+    调整给定图像列表的分辨率。
+
+    Args:
+        images (List[PIL.Image.Image]): 需要调整大小的PIL图像列表。
+        size (tuple): 一个包含宽度和高度的目标尺寸元组，例如(800, 600)。
+
+    Returns:
+        resized_images (List[PIL.Image.Image]): 调整过分辨率的图像列表。
+    """
+
+    resized_images = []
+
+    for img in images:
+        # 使用ANTIALIAS算法来保证缩放质量
+        resized_img = img.resize(size, Image.Resampling.LANCZOS)
+        resized_images.append(resized_img)
+
+    return resized_images
 
 
 class DeepSeekVL2VRAG:
@@ -19,8 +42,10 @@ class DeepSeekVL2VRAG:
     ):
 
         # specify the path to the model
+        min_pixels = 256 * 28 * 28
+        max_pixels = 1280 * 28 * 28
         self.vl_chat_processor: DeepseekVLV2Processor = (
-            DeepseekVLV2Processor.from_pretrained(checkpoint_path)
+            DeepseekVLV2Processor.from_pretrained(checkpoint_path, min_pixels=min_pixels, max_pixels=max_pixels)
         )
         self.tokenizer = self.vl_chat_processor.tokenizer
 
@@ -32,13 +57,11 @@ class DeepSeekVL2VRAG:
     def inference(
         self,
         conversation: list,
-        image_index_folder: pathlib.Path = None,
-        text_emb_folder: pathlib.Path = None,
-        use_pics: int = 0,
     ):
 
         # load images and prepare for inputs
         pil_images = load_pil_images(conversation)
+        pil_images = resize_images(pil_images, (768, 768))
         prepare_inputs = self.vl_chat_processor(
             conversations=conversation,
             images=pil_images,
@@ -67,6 +90,13 @@ class DeepSeekVL2VRAG:
         # print(f"{prepare_inputs['sft_format'][0]}", answer)
         return answer
 
+    def inference_rag(
+        self,
+        image_index_folder: pathlib.Path = None,
+        text_emb_folder: pathlib.Path = None,
+        use_pics: int = 0
+    ):
+        pass
 
 if __name__ == "__main__":
     image_path = "./data/Classic Images/DR/mild NPDR_1.jpeg"
