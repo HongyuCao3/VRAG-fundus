300fea01fc742cb784023db7434f88919d878678
HongyuCao <pb18111726@mail.ustc.edu.cn>
2025-02-17 16:15:04 +0800

    finish framework, cannot fix torch

--- DeepSeekVL2VRAG/deepseekvl2_vrag.py
+++ DeepSeekVL2VRAG/deepseekvl2_vrag.py
@@ -13,6 +13,13 @@ from DeepSeekVL2VRAG.DeepSeek_VL2.deepseek_vl2.utils.io import load_pil_images
 
 from PIL import Image
 
+from fundus_knowledge_base.index_manager.mulit_disease_index_manager import (
+    MultiDiseaseIndexManager,
+)
+from PathManager.EmbPathManager import EmbPathManager, EmbPathConfig
+from fundus_knowledge_base.knowledge_retriever.TextRetriever import TextRetriever
+
+
 def resize_images(images: list[Image.Image], size: tuple) -> list[Image.Image]:
     """
     调整给定图像列表的分辨率。
@@ -45,7 +52,9 @@ class DeepSeekVL2VRAG:
         min_pixels = 256 * 28 * 28
         max_pixels = 1280 * 28 * 28
         self.vl_chat_processor: DeepseekVLV2Processor = (
-            DeepseekVLV2Processor.from_pretrained(checkpoint_path, min_pixels=min_pixels, max_pixels=max_pixels)
+            DeepseekVLV2Processor.from_pretrained(
+                checkpoint_path, min_pixels=min_pixels, max_pixels=max_pixels
+            )
         )
         self.tokenizer = self.vl_chat_processor.tokenizer
 
@@ -90,13 +99,87 @@ class DeepSeekVL2VRAG:
         # print(f"{prepare_inputs['sft_format'][0]}", answer)
         return answer
 
+    def build_prompt(
+        self,
+        query: str,
+        image_context: str = None,
+        text_context: str = None,
+        diagnosis_standard: str = None,
+    ):
+        parts = []
+        if diagnosis_standard:
+            parts.append(f"Diagnosing Standard: {diagnosis_standard}\n")
+        if image_context:
+            parts.append(
+                f"The possible diagnosing level and similarity: {image_context}\n"
+            )
+        if text_context:
+            parts.append(f"The possible diagnosis and similarity: {text_context}\n")
+        parts.append(query)
+
+        return "".join(parts)
+
     def inference_rag(
         self,
+        conversation,
         image_index_folder: pathlib.Path = None,
         text_emb_folder: pathlib.Path = None,
-        use_pics: int = 0
+        use_pics: int = 0,
     ):
-        pass
+        if image_index_folder:
+            self.index_manager = MultiDiseaseIndexManager()
+            self.image_index = self.index_manager.load_index(image_index_folder)
+        if text_emb_folder:
+            self.text_embedding = TextRetriever(emb_folder=text_emb_folder)
+        conversation_rag = []
+        for message in conversation:
+            if message["role"] == "<|User|>":  # only vrag for user
+                image_path = message["images"][0]
+                query = message["content"]
+            else:
+                conversation_rag.append(message)  # remain original for assitant
+                continue
+            if image_index_folder:
+                retrieved_images = self.index_manager.retrieve_image(
+                    self.image_index, img_path=image_path, top_k=1
+                )
+            if text_emb_folder:
+                retrieved_texts = self.text_embedding.retrieve(input_img=image_path)
+            images_content = [message["images"][0]]
+            if image_index_folder:  # multi image as input
+                for i in range(use_pics):
+                    # for img in retrieved_images.img:
+                    images_content.append(retrieved_images.img[i])
+            # form prompt
+            if image_index_folder:
+                image_context = " ".join(
+                    [
+                        f"{txt}: {img}"
+                        for txt, img in zip(
+                            retrieved_images.txt, retrieved_images.score
+                        )
+                    ]
+                )
+            else:
+                image_context = None
+            if text_emb_folder:
+                text_context = " ".join(
+                    [
+                        f"{txt}: {img}"
+                        for txt, img in zip(retrieved_texts.txt, retrieved_texts.score)
+                    ]
+                )
+            else:
+                text_context = None
+            prompt = self.build_prompt(
+                query=query, image_context=image_context, text_context=text_context
+            )
+            message["content"] = prompt
+            conversation_rag.append(message)
+        print(conversation_rag)
+        answer = self.inference(conversation_rag)
+        return answer
+
 
 if __name__ == "__main__":
     image_path = "./data/Classic Images/DR/mild NPDR_1.jpeg"
@@ -110,5 +193,15 @@ if __name__ == "__main__":
         {"role": "<|Assistant|>", "content": ""},
     ]
     dsvl2 = DeepSeekVL2VRAG()
+    pm = EmbPathManager()
+    text_emb_folder = pm.get_emb_dir(pm.config.default_text_emb_name)
+    image_index_folder = pathlib.Path(
+        "./fundus_knowledge_base/emb_savings/mulit_desease_image_index"
+    )
     answer = dsvl2.inference(conversation=conversation)
+    # answer_rag = dsvl2.inference_rag(
+    #     conversation=conversation,
+    #     text_emb_folder=text_emb_folder,
+    #     image_index_folder=image_index_folder,
+    # )
     print(answer)
