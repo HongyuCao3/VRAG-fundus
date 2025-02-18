8e3a2fa7e2d61c337cb8ccd21c93df29bdc47b0f
HongyuCao <pb18111726@mail.ustc.edu.cn>
2025-02-18 13:58:41 +0800

    make vrag name args

--- AnalysisVRAG/main.py
+++ AnalysisVRAG/main.py
@@ -11,18 +11,18 @@ from AnalysisVRAG.multi_modal_vqa import (
 )
 
 
-def analyze_multi_modal_classification():
+def analyze_multi_modal_classification(vrag_name: str):
     pm = EmbPathManager()
     text_emb_folder = pm.get_emb_dir(pm.config.default_text_emb_name)
     image_index_folder = pathlib.Path(
         "./fundus_knowledge_base/emb_savings/mulit_desease_image_index"
     )
-    text_emb_name = text_emb_folder.name
-    # text_emb_name = None
+    # text_emb_name = text_emb_folder.name
+    text_emb_name = None
     image_index_name = image_index_folder.name
     # image_index_name = None
     use_pics = True
-    result_saving_path = f"./QwenVLVRAG/output/Multimodal VQA Dataset/{image_index_name}_{text_emb_name}_usepics_{use_pics}_-1.json"
+    result_saving_path = f"./{vrag_name}/output/Multimodal VQA Dataset/{image_index_name}_{text_emb_name}_usepics_{use_pics}_-1.json"
     image_saving_path = result_saving_path.replace("output", "output/analysis")
     image_saving_path = image_saving_path.replace("json", "png")
     word_cloud_saving_path = image_saving_path.replace("png", "_word_cloud.png")
@@ -33,7 +33,7 @@ def analyze_multi_modal_classification():
     mmca.plot_word_cloud_comparsion(wordcloud_saving_path=word_cloud_saving_path)
 
 
-def analyze_multi_modal_vqa(key_word: str = None):
+def analyze_multi_modal_vqa(vrag_name: str, key_word: str = None):
     pm = EmbPathManager()
     text_emb_folder = pm.get_emb_dir(pm.config.default_text_emb_name)
     image_index_folder = pathlib.Path(
@@ -43,7 +43,7 @@ def analyze_multi_modal_vqa(key_word: str = None):
     # text_emb_name = None
     image_index_name = image_index_folder.name
     # image_index_name = None
-    result_saving_path = f"./QwenVLVRAG/output/Multimodal VQA Dataset_VQA/{image_index_name}_{text_emb_name}_usepics_0_-1.json"
+    result_saving_path = f"./{vrag_name}/output/Multimodal VQA Dataset_VQA/{image_index_name}_{text_emb_name}_usepics_0_-1.json"
     analysis_config = MultiModalVQAAnalysisConfig()
     analysis_saving_path = result_saving_path.replace("output", "output/analysis")
     if key_word is None:
@@ -74,5 +74,5 @@ if __name__ == "__main__":
     # cm = analysis.calculate_confusion_matrix(args.res_path)
     # print(f"Accuracy: {analysis.calculate_accuracy():.4f}")
     # print(cm)
-    analyze_multi_modal_classification()
+    analyze_multi_modal_classification(vrag_name="DeepSeekVL2VRAG")
     # analyze_multi_modal_vqa(key_word="epiretinal membrane")
